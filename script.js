7a93ce853957020c2af5b11a79a85e358eae08ae
Misbahur rahman <98620184+Misbahrahman@users.noreply.github.com>
2023-12-21 19:57:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,8 +17,14 @@ const library = [
 ];
 
 const numberOfBooksRead = () => {
-  // write your code here
+  let ans = 0;
+  library.forEach(function(ele){
+	  if(ele.readingStatus)ans++;
+  });
+	
+	return ans;
 };
+
 
 // Do not change the code below
 
